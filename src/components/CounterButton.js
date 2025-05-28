ed8793c872bda4d4d4f2ba8d7ba2ff8dd01758f7
bpmdevr <bpolatmd@gmail.com>
2025-05-28 14:57:48 +0300

    prettier test

---
+++ src/components/CounterButton.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+const CounterButton = (props) => {
+   const [count, setCount] = useState(0)
+
+   return(
+    <button
+                      id="counter"
+   color={props.color}    onClick={() => setCount((count) => count + 1)}>
+                             Count: {count}
+               </button>
+   )
+          }
+
+     export default CounterButton
